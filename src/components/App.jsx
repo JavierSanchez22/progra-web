8471118143cdb336ccfc3c2f1876ecb9539eb5a2
Javier <fjsanchez@correo.url.edu.gt>
2025-08-15 21:12:57 -0600

    feat: connect to PokeApi and show the pokemons in page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,46 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [pokemon, setPokemon] = useState([])
+
+  const fetchPokemons = async () => {
+    try {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
+      const data = await response.json()
+
+      const pokemonDetails = await Promise.all(
+        data.results.map(async (pokemon) => {
+          const pokemonResponse = await fetch(pokemon.url)
+          const pokemonData = await pokemonResponse.json()
+          console.log('Pokemon Name: ', pokemonData.name)
+          return pokemonData
+        })
+      )
+      
+      setPokemon(pokemonDetails)
+    } catch (error) {
+      console.error('Error fetching pokemons: ', error)
+    }
+  }
+
   return (
-    <>
-      <h1>Hi</h1>
-    </>
+    <div className='AppPokemon'>
+      <h1>Mi pokedex</h1>
+      <button onClick={fetchPokemons}>Cargar Pokemons</button>
+      <p>Pokemons encontrados: {pokemon.length}</p>
+      <div>
+        {pokemon.map((poke) => (
+          <div key={poke.id} className='card'>
+            <h2>{poke.name}</h2>
+            <img src={poke.sprites.front_default} alt={poke.name} />
+            <p>Altura: {poke.height}</p>
+            <p>Peso: {poke.weight}</p>
+            <p>Tipo: {poke.types.map((type) => type.type.name).join(', ')}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
-
 export default App
