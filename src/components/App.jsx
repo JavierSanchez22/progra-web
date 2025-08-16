276c1cbded449eb5e5271ee5b30a99aaa1a08f4a
Javier <fjsanchez@correo.url.edu.gt>
2025-08-16 13:37:51 -0600

    feat: add buttons next/prev

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,18 +3,27 @@ import './App.css'
 
 function App() {
   const [pokemon, setPokemon] = useState([])
+  const [currentPage, setCurrentPage] = useState(0)
+  const pokemonPerPage = 20
+  const totalPages = Math.ceil(pokemon.length / pokemonPerPage)
+
+  const nextPage = () => setCurrentPage(prev => Math.min(prev + 1, totalPages - 1))
+  const prevPage = () => setCurrentPage(prev => Math.max(prev - 1, 0))
+
+  const getCurrentPokemons = () => {
+    const startIndex = currentPage * pokemonPerPage
+    return pokemon.slice(startIndex, startIndex + pokemonPerPage)
+  }
 
   const fetchPokemons = async () => {
     try {
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=200')
       const data = await response.json()
 
       const pokemonDetails = await Promise.all(
         data.results.map(async (pokemon) => {
           const pokemonResponse = await fetch(pokemon.url)
-          const pokemonData = await pokemonResponse.json()
-          console.log('Pokemon Name: ', pokemonData.name)
-          return pokemonData
+          return await pokemonResponse.json()
         })
       )
       
@@ -39,6 +48,11 @@ function App() {
           <p>Tipo: {poke.types.map((type) => type.type.name).join(', ')}</p>
         </div>
         ))}
+      </div>
+      <div className="controls">
+        <button onClick={prevPage} disabled={currentPage === 0}>Prev</button>
+        <span>Página {currentPage +1} de {totalPages}</span>
+        <button onClick={nextPage} disabled={currentPage === totalPages - 1}>Next</button>
       </div>
     </div>
   )
