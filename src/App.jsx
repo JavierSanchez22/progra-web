bc288a8a6a86d5930da6cd682fbbe7f599ddffa9
JavierSanchez22 <fjsanchez@correo.url.edu.gt>
2025-08-26 19:53:33 -0600

    feat: principal function added, add task and show this

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
-import './App.css'
+import useLocalStorage from './hooks/useLocalStorage';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
+import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useLocalStorage('tasks', []);
+
+  const addTask = (newTask) => {
+    setTasks(prevTasks => [...prevTasks, newTask]);
+  };
+
+  const toggleComplete = (taskId) => {
+    setTasks(prevTasks =>
+      prevTasks.map(task =>
+        task.id === taskId
+          ? { ...task, completed: !task.completed }
+          : task
+      )
+    );
+  };
+
+  const deleteTask = (taskId) => {
+    setTasks(prevTasks => prevTasks.filter(task => task.id !== taskId));
+  };
+
   return (
     <div className="app">
       <div className="container">
-        <h1>TASK MANAGER</h1>
+        <h1>Task Manager</h1>
+        
+        <TaskForm onAddTask={addTask} />
+
+        <TaskList
+          tasks={tasks}
+          onToggleComplete={toggleComplete}
+          onDelete={deleteTask}
+        />
       </div>
     </div>
   );
