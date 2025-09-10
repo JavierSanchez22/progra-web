df2c6abdd130821658882507571528d1eb70987c
JavierSanchez22 <fjsanchez@correo.url.edu.gt>
2025-09-10 01:30:14 -0600

    feat: add User model with initial migration

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# First Model - Author
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    nationality = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f"{self.name} {self.last_name}"
+    
+    class Meta:
+        verbose_name = "Author"
+        verbose_name_plural = "Authors"
