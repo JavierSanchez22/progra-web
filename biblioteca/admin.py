9c946d392b5259c7fb0299fd5c0f058e8d5baf18
JavierSanchez22 <fjsanchez@correo.url.edu.gt>
2025-09-10 01:50:40 -0600

    feat: configure Editorial admin interface

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from .models import Author
+from .models import Editorial
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ['name', 'last_name', 'nationality', 'birth_date']
     list_filter = ['nationality', 'birth_date']
     search_fields = ['name', 'last_name']
+
+@admin.register(Editorial)
+class EditorialAdmin(admin.ModelAdmin):
+    list_display = ['name', 'founder', 'phone', 'email']
+    list_filter = ['founder']
+    search_fields = ['name']
+    ordering = ['name']
+    autocomplete_fields = ['founder']
